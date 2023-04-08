d8a1bb68f5f86690b8b84901a2b061b07ed8a4b5
Zaur <zaur.cherchesov@mail.ru>
2023-04-08 16:26:45 +0300

    finished task 2

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿int numberA = int.Parse (Console.ReadLine());
+int numberB = int.Parse (Console.ReadLine());
+
+if (numberA > numberB)
+{
+    Console.Write("max = ");
+    Console.Write(numberA);
+}
+else
+{
+    Console.Write("max = ");
+    Console.Write(numberB);
+}
